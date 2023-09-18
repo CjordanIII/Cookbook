883f03f0f337c80be2b519e73710ff476d94d0a4
Clarence Jordan <clarencejordan087@gmail.com>
2023-09-18 14:35:51 -0500

    first push

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,22 @@
+import * as userService from "../utils/users-service";
+
 const { Link } = require("react-router-dom");
-import * as userService from '../utils/users-service';
 
-
-
-
-function NavBar({user,setUser}){
-    const handleLogout =()=>{
-      userService.logOut()
-      setUser(null)
-    }
-    return (
-      <nav>
-        <h3>Welcome {user.name}</h3>
-        <Link to="/orders"> Orders</Link>&nbsp;|&nbsp;
-        <Link to="/orders/new">New Order</Link>&nbsp;
-        <Link to="" onClick={handleLogout}>
-          Log out
-        </Link>
-      </nav>
-    );
+function NavBar({ user, setUser }) {
+  const handleLogout = () => {
+    userService.logOut();
+    setUser(null);
+  };
+  return (
+    <nav>
+      <h3>Welcome {user.name}</h3>
+      <Link to="/orders"> Orders</Link>&nbsp;|&nbsp;
+      <Link to="/orders/new">New Order</Link>&nbsp;
+      <Link to="" onClick={handleLogout}>
+        Log out
+      </Link>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
